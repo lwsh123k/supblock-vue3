a92db6c09e70f6bf1a77f357dc175082bbe94ec2
lwsh123k <lwsh123k@163.com>
2025-01-13 11:38:27 +0800

    change to ws

--- src/ethers/provider.ts
+++ src/ethers/provider.ts
@@ -1,7 +1,9 @@
 import { ethers } from 'ethers';
 
 // 提供provider, 合约读写实例
-const provider = new ethers.providers.JsonRpcProvider('http://127.0.0.1:8545');
-provider.pollingInterval = 1000;
+// const provider = new ethers.providers.JsonRpcProvider('http://127.0.0.1:8545');
+// provider.pollingInterval = 1000;
+
+const provider = new ethers.providers.WebSocketProvider('ws://127.0.0.1:8545');
 
 export { provider };
